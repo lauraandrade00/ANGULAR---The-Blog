7b93a36cd59f0f407dea716a512b2dcec024bfe4
Laura Andrade <lauraandradecamp@gmail.com>
2022-12-12 19:48:17 -0300

    routing and dynamic parts done

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "saiu novo filme",
+        "description": "ola",
+        "photoCover": "vvv"
+    },
+    {
+        "id": "2",
+        "title": "saiu novo filme",
+        "description": "ola",
+        "photoCover": "vvv"
+    }
+];
